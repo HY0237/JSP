e874b212e2dc94c4748ec0ac08f921ea3e02c93c
HyeYeong <hy0237@uab.edu>
2021-10-01 19:20:59 +0900

    file upload

--- JSPprj/src/main/java/com/newlecture/web/controller/admin/notice/ListController.java
+++ JSPprj/src/main/java/com/newlecture/web/controller/admin/notice/ListController.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -30,19 +31,31 @@ public class ListController extends HttpServlet {
 		String[] openIds = request.getParameterValues("open-id");
 		String[] delIds = request.getParameterValues("del-id");
 		String cmd = request.getParameter("cmd");
+		String ids_ = request.getParameter("ids");
+		String[] ids = ids_.trim().split(" ");
+		
+		NoticeService service = new NoticeService();
 		
 		switch(cmd) {
 		case "老褒傍俺":
 			for(String openId : openIds)
 				System.out.printf("open id : %s\n ", openId);
-			break;
-		case "老褒昏力":
-			NoticeService service = new NoticeService();
-			int[] ids = new int[delIds.length];
-			for(int i =0; i < delIds.length; i++)
-				ids[i] =Integer.parseInt(delIds[i]);
 			
-			int result = service.deleteNoticeAll(ids);
+			List<String> oids = Arrays.asList(openIds);
+			
+			List<String> cids = new ArrayList(Arrays.asList(ids));
+			cids.removeAll(oids);
+			
+			service.pubNoticeAll(oids, cids);
+		
+			break;
+			
+		case "老褒昏力":
+			int[] ids1 = new int[delIds.length];
+			for(int i =0; i < delIds.length; i++)
+				ids1[i] =Integer.parseInt(delIds[i]);
+			
+			int result = service.deleteNoticeAll(ids1);
 			break;
 		}
 		
--- JSPprj/src/main/java/com/newlecture/web/service/NoticeService.java
+++ JSPprj/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -20,15 +20,59 @@ public class NoticeService {
 		
 		return 0;
 	}
-	public int pubNoticeAll(int[] ids){
+	public int pubNoticeAll(int[] oids, int[] cids){
 		
-		return 0;
+		List<String> oidsList = new ArrayList<>();
+		for(int i = 0; i < oids.length; i++)
+			oidsList.add(String.valueOf(oids[i]));
+		
+		List<String> cidsList = new ArrayList<>();
+		for(int i = 0; i < cids.length; i++)
+			cidsList.add(String.valueOf(cids[i]));
+			
+		return pubNoticeAll(oidsList, cidsList);
 	}
+	public int pubNoticeAll(List<String> oids, List<String> cids){
+		
+		String oidsCSV = String.join(",", oids);
+		String cidsCSV = String.join(",", cids);
+		
+		return pubNoticeAll(oidsCSV, cidsCSV);
+	}
+	public int pubNoticeAll(String oidsCSV, String cidsCSV){
+		
+		int result = 0;
+		String sqlOpen = String.format("UPDATE NOTICE SET PUB=1 WHERE ID IN (%s)", oidsCSV);
+		String sqlClose = String.format("UPDATE NOTICE SET PUB=0 WHERE ID IN (%s)", cidsCSV);
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url,"newlec", "1234");
+			Statement stOpen = con.createStatement();
+			result += stOpen.executeUpdate(sqlOpen);
+			
+			Statement stClose = con.createStatement();
+			result += stClose.executeUpdate(sqlClose);
+			
+			stOpen.close();
+			stClose.close();
+			con.close(); 
+		} catch (ClassNotFoundException | SQLException e) {
+			
+			e.printStackTrace();
+		}
+	
+		return result;
+	}
+	
 	public int insertNotice(Notice notice){
 		int result = 0;
 		
 			
-		String sql = "INSERT INTO NOTICE(TITLE, CONTENT, WRITER_ID, PUB) VALUES(?,?,?,?) ";
+		String sql = "INSERT INTO NOTICE(TITLE, CONTENT, WRITER_ID, PUB, FILES) VALUES(?,?,?,?,?) ";
 		
 		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
 		
@@ -41,7 +85,8 @@ public class NoticeService {
 			st.setString(2, notice.getContent());
 			st.setString(3, notice.getWriterId());
 			st.setBoolean(4, notice.getPub());
-	
+			st.setString(5, notice.getFiles());
+			
 			result = st.executeUpdate();
 			
 			st.close();
@@ -135,6 +180,67 @@ public class NoticeService {
 		
 		return list;
 	}
+	
+	public List<NoticeView> getNoticePubList(String field, String query, int page) {
+		
+		List<NoticeView> list = new ArrayList<>();
+		
+		String sql = "SELECT * FROM ("
+				+ "    SELECT ROWNUM NUM, N. *"
+				+ "    FROM (SELECT * FROM NOTICE_CMT_VIEW WHERE "+field+" LIKE ? ORDER BY REGDATE DESC) N"
+				+ ") "
+				+ "WHERE PUB = 1 AND NUM BETWEEN ? AND ?";
+		
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url,"newlec", "1234");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, "%"+query+"%");
+			st.setInt(2, 1+(page-1)*2);
+			st.setInt(3, page*2);
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()){
+
+				int id = rs.getInt("ID");
+				String title =rs.getString("TITLE");
+				Date regdate =rs.getDate("REGDATE");
+				String writerId =rs.getString("WRITER_ID");
+				String hit =rs.getString("HIT");
+				String files =rs.getString("FILES");
+				int cmtCount = rs.getInt("CMT_COUNT");
+				boolean pub = rs.getBoolean("PUB");
+				
+				NoticeView notice = new NoticeView(
+						id,
+						title,
+						regdate,
+						writerId,
+						hit,
+						files,
+						pub,
+						cmtCount
+						);
+				list.add(notice);
+			} 
+
+
+			rs.close();
+			st.close();
+			con.close(); 
+		} catch (ClassNotFoundException | SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		return list;
+	}
+	
 	public int getNoticeCount() {
 		
 		return getNoticeCount("title", "") ;
@@ -366,5 +472,6 @@ public class NoticeService {
 		}
 		return result;
 	}
+
 
 }
